debb0cf58ed70234243f67c626ca20e178d41b66
nadhembenameur99 <nadhem.benameur@enis.tn>
2021-09-22 09:03:37 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MemberListComponent } from './member-list/member-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemberListComponent
+  ],
+  imports: 
