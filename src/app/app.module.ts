91664bbb7cab87cf2deb8772be269d88c8d7adfb
nadhembenameur99 <nadhem.benameur@enis.tn>
2021-09-29 08:40:25 +0100

    nba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import {AppComponent} from './app.component';
+import {MemberListComponent} from './member-list/member-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     MemberListComponent
   ],
-  imports: 
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTableModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
